8e1923498bfa42080de7a6c25640b3c59da503b0
Андрей Щиголь <andryushka05@gmail.com>
2024-02-27 15:47:58 +0200

    Reading args & Documentation

--- Program.cs
+++ Program.cs
@@ -10,6 +10,29 @@ namespace Markdown
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: <inputFile> [--out <outputFile>]");
+                return;
+            }
+
+            string inputFile = args[0];
+            string outputFile = null;
+
+            if (args.Length >= 3 && args[1] == "--out")
+            {
+                outputFile = args[2];
+            }
+
+            if (outputFile != null)
+            {
+
+            }
+            else
+            {
+
+            }
+
         }
     }
 }
